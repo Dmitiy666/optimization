230668af1990e791ea9f542b37160ed322dd2b1c
235382 <y6wb@gtsgs>
2024-12-20 20:40:16 +0300

    Second task №"4.1

---
+++ task3.py
@@ -0,0 +1,141 @@
+import os
+
+import optuna
+from optuna.trial import TrialState
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import torch.utils.data
+from torchvision import datasets
+from torchvision import transforms
+
+from train import train
+
+DEVICE = torch.device("cpu")
+BATCHSIZE = 128
+CLASSES = 10
+DIR = os.getcwd()
+EPOCHS = 10
+N_TRAIN_EXAMPLES = BATCHSIZE * 30
+N_VALID_EXAMPLES = BATCHSIZE * 10
+
+
+def define_model(trial):
+    # We optimize the number of layers, hidden units and dropout ratio in each layer.
+    dr_out = trial.suggest_int("dropout", 0.2, 0.5)
+    conv_layers = []
+    conv_layers.append(nn.Conv2d(3, 16, kernel_size=3, padding=1))
+    conv_layers.append(nn.ReLU())
+    conv_layers.append(nn.MaxPool2d(2, 2))
+
+    model_layers = []
+    model_layers.extend(conv_layers)
+    model_layers.append(nn.Flatten())
+
+    in_features = 16 * 16 * 16
+    for i in range(dr_out):
+        out_features = trial.suggest_int("dropout{}".format(i), 32, 512)
+        model_layers.append(nn.Linear(in_features, out_features))
+        model_layers.append(nn.ReLU())
+        p = trial.suggest_float("dropout_l{}".format(i), 0.2, 0.5)
+        model_layers.append(nn.Dropout(p))
+
+        in_features = out_features
+
+    model_layers.append(nn.Linear(in_features, CLASSES))
+    model_layers.append(nn.LogSoftmax(dim=1))
+
+    return nn.Sequential(*model_layers)
+
+
+def get_cifar10():
+    # Load FashionMNIST dataset.
+    transform = transforms.Compose([transforms.ToTensor()])
+    train_loader = torch.utils.data.DataLoader(
+        datasets.CIFAR10(DIR, train=True, download=True, transform=transform),
+        batch_size=BATCHSIZE,
+        shuffle=True,
+    )
+    valid_loader = torch.utils.data.DataLoader(
+        datasets.CIFAR10(DIR, train=False, transform=transform),
+        batch_size=BATCHSIZE,
+        shuffle=True,
+    )
+
+    return train_loader, valid_loader
+
+
+def objective(trial):
+    # Generate the model.
+    model = define_model(trial).to(DEVICE)
+
+    # Generate the optimizers.
+    optimizer_name = trial.suggest_categorical("optimizer", ["Adam", "RMSprop", "SGD"])
+    lr = trial.suggest_float("lr", 1e-5, 1e-1, log=True)
+    optimizer = getattr(optim, optimizer_name)(model.parameters(), lr=lr)
+
+    # Get the FashionMNIST dataset.
+    train_loader, valid_loader = get_cifar10()
+
+    # Training of the model.
+    for epoch in range(EPOCHS):
+        model.train()
+        for batch_idx, (data, target) in enumerate(train_loader):
+            # Limiting training data for faster epochs.
+            if batch_idx * BATCHSIZE >= N_TRAIN_EXAMPLES:
+                break
+
+            data, target = data.to(DEVICE), target.to(DEVICE)
+
+            optimizer.zero_grad()
+            output = model(data)
+            loss = F.nll_loss(output, target)
+            loss.backward()
+            optimizer.step()
+
+        # Validation of the model.
+        model.eval()
+        correct = 0
+        with torch.no_grad():
+            for batch_idx, (data, target) in enumerate(valid_loader):
+                # Limiting validation data.
+                if batch_idx * BATCHSIZE >= N_VALID_EXAMPLES:
+                    break
+                data, target = data.to(DEVICE), target.to(DEVICE)
+                output = model(data)
+                # Get the index of the max log-probability.
+                pred = output.argmax(dim=1, keepdim=True)
+                correct += pred.eq(target.view_as(pred)).sum().item()
+
+        accuracy = correct / min(len(valid_loader.dataset), N_VALID_EXAMPLES)
+
+        trial.report(accuracy, epoch)
+
+        # Handle pruning based on the intermediate value.
+        if trial.should_prune():
+            raise optuna.exceptions.TrialPruned()
+
+    return accuracy
+
+
+if __name__ == "__main__":
+    study = optuna.create_study(direction="maximize")
+    study.optimize(objective, n_trials=100, timeout=600)
+
+    pruned_trials = study.get_trials(deepcopy=False, states=[TrialState.PRUNED])
+    complete_trials = study.get_trials(deepcopy=False, states=[TrialState.COMPLETE])
+
+    print("Study statistics: ")
+    print("  Number of finished trials: ", len(study.trials))
+    print("  Number of pruned trials: ", len(pruned_trials))
+    print("  Number of complete trials: ", len(complete_trials))
+
+    print("Best trial:")
+    trial = study.best_trial
+
+    print("  Value: ", trial.value)
+
+    print("  Params: ")
+    for key, value in trial.params.items():
+        print("    {}: {}".format(key, value))
